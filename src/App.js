6e9803f8d5f3ac1fe0ff649c71fe5fcee6b96a4f
sandoqarahma <rahma.san.m@gmail.com>
2019-10-10 09:35:00 +0300

    beforeformat the pc 10-10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import 
+import React, { Component } from "react";
+import Table from "./components/Table";
 export default class App extends Component {
   state = {
     repos: [
       {
         id: 1,
-        title: 'Array',
-        status: 'Private',
-        language: 'HTML'
+        title: "Array",
+        status: "Private",
+        language: "HTML"
       },
       {
         id: 2,
-        title: 'Object',
-        status: 'Public',
-        language: 'JavaScript'
+        title: "Object",
+        status: "Public",
+        language: "JavaScript"
       }
     ]
   };
   render() {
     return (
-      <div style={{ border: 'black 1px solid' }}>
+      <div style={{ border: "black 1px solid" }}>
         <h6>App</h6>
+        <Table reposTable={this.state.repos} />
       </div>
     );
   }
--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Repo extends Component {
   render() {
     return (
-      <div style={{ border: '3px red solid' }}>
-        <h6>Repo</h6>
+      <div style={{ border: "3px red solid" }}>
+
+        <tr>
+          
+          <td>{this.props.id}</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+
+        </tr>
+
+        <p>{this.props.repo.title}</p>
+        <p>{this.props.repo.id}</p>
       </div>
     );
   }
